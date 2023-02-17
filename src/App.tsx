04334d15d1d679b8a79520427f363f3e3c21a8c9
ChravisDeYoung <dejong_travis@yahoo.ca>
2023-02-16 23:17:27 -0500

    added skip and removes test after success

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 import jsonData from "./data/tests.json";
@@ -10,15 +10,17 @@ type RAT = {
 };
 
 function App() {
-  const [test, setTest] = useState<RAT>(
-    jsonData[Math.floor(Math.random() * jsonData.length)]
+  const tests = useRef<Array<RAT>>(jsonData);
+  const [testIndex, setTestIndex] = useState(
+    Math.floor(Math.random() * tests.current.length)
   );
   const [answer, setAnswer] = useState<string>("");
 
   const checkAnswer = () => {
-    if (answer.trim() === test.solution) {
+    if (answer.trim() === tests.current[testIndex].solution) {
       alert("winner!");
-      setTest(jsonData[Math.floor(Math.random() * jsonData.length)]);
+      tests.current.splice(testIndex, 1);
+      setTestIndex(Math.floor(Math.random() * tests.current.length));
     } else {
       alert("oops, try again...");
     }
@@ -29,11 +31,14 @@ function App() {
   return (
     <div className="App">
       <h1>Remote Association Test</h1>
-      <div className="card">
-        {test.items.map((x, i) => (
+      <div
+        className="card"
+      >
+        {tests.current[testIndex].items.map((x, i) => (
           <p key={i}>{x}</p>
         ))}
       </div>
+
       <div className="card">
         <input
           type="text"
@@ -41,6 +46,16 @@ function App() {
           onChange={(e) => setAnswer(e.target.value)}
         ></input>
         <button onClick={checkAnswer}>Check</button>
+      </div>
+
+      <div className="card">
+        <button
+          onClick={() =>
+            setTestIndex(Math.floor(Math.random() * tests.current.length))
+          }
+        >
+          Skip
+        </button>
       </div>
     </div>
   );
