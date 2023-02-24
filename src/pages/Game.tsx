d19a0e227be1538f3355997c6b6e00c76a218556
ChravisDeYoung <dejong_travis@yahoo.ca>
2023-02-23 22:45:56 -0500

    style changes to Game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-import jsonData from "../data/tests.json";
+import jsonData from "../data/tests-sample.json";
+import logo from "../assets/rat-logo.png";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type RAT = {
@@ -51,6 +52,12 @@ function GamePage() {
     }
   };
 
+  const inputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      checkAnswer();
+    }
+  };
+
   useEffect(() => {
     if (gameClock > 0) {
       setTimeout(() => {
@@ -61,43 +68,77 @@ function GamePage() {
     }
   }, [gameClock]);
 
-  return gameClock > 0 ? (
-    <div className="card">
-      <h2>Time Left: {gameClock}</h2>
-      <div className="card">
-        <h3>{score}</h3>
-        {tests.current[testIndex].items.map((x, i) => (
-          <p
-            key={i}
-            style={{
-              color: difficultyMapping[tests.current[testIndex].difficulty],
-            }}
-          >
-            {x}
-          </p>
-        ))}
-      </div>
+  return gameClock > 0 && tests.current.length > 0 ? (
+    <>
+      <div>
+        <h3 className="font-semibold text-3xl mt-5">{score}</h3>
 
-      <div className="card">
+        <div
+          className="bg-yellow-600 rounded-full h-20 w-20 mx-auto my-10"
+          style={{
+            backgroundImage:
+              "conic-gradient(rgb(253 224 71)" +
+              (100 - Math.round((gameClock / 30) * 100)) +
+              "%, rgb(202 138 4) " +
+              (100 - Math.round((gameClock / 30) * 100)) +
+              "%)",
+          }}
+        >
+          <h1 className="font-bold text-[3rem]">{gameClock}</h1>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div>
+            <div className="bg-gray-300 rounded-full h-20 w-20 inline-block relative top-[1.6rem] right-[30%]"></div>
+            <div className="bg-gray-300 rounded-full h-20 w-20 inline-block relative top-[1.6rem] left-[30%]"></div>
+          </div>
+          {tests.current[testIndex].items.map((x, i) => (
+            <p
+              key={i}
+              // style={{
+              //   color: difficultyMapping[tests.current[testIndex].difficulty],
+              // }}
+              className="bg-gray-300 font-medium py-2 w-1/2 my-[0.1rem] text-xl"
+            >
+              {x}
+            </p>
+          ))}
+        </div>
+
         <input
+          className="bg-gray-300 font-medium py-2 w-1/2 my-5 text-center focus:outline-black text-xl"
           type="text"
           value={answer}
           onChange={(e) => setAnswer(e.target.value)}
+          onKeyDown={inputKeyDown}
         ></input>
-        <button onClick={checkAnswer}>Check</button>
       </div>
-
-      <div className="card">
+      <div>
         <button disabled={tests.current.length < 1} onClick={skipTest}>
           Skip
         </button>
       </div>
-    </div>
+    </>
   ) : (
-    <div className="card">
-      <h2>Game Over</h2>
-      <h3>Score: {score}</h3>
-      <Link to="/">Return to Home</Link>
+    <div>
+      <div>
+        <img className="my-5 mx-auto w-3/4" src={logo} />
+        <h1 className="font-bold text-5xl tracking-widest">Game Over</h1>
+      </div>
+
+      <div className="my-10">
+        <h2 className="font-semibold mb-1 text-xl">score</h2>
+        <span className="font-bold text-5xl">{score}</span>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <Link className="bg-gray-300 font-medium py-2 w-1/2 my-2" to="/game">
+          play again
+        </Link>
+        <Link className="bg-gray-300 font-medium py-2 w-1/2 my-2" to="/">
+          home
+        </Link>
+      </div>
     </div>
   );
 }
