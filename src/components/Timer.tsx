a1215334aaf45b9c54e777b97519bb00390eac94
ChravisDeYoung <dejong_travis@yahoo.ca>
2023-02-28 23:29:33 -0500

    created Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Timer(props: { timeInSeconds: number }) {
+  const [timeLeft, setTimeLeft] = useState<number>(props.timeInSeconds);
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+    }
+  }, [timeLeft]);
+
+  return (
+    <div
+      className="rounded-full h-20 w-20 mx-auto my-10"
+      style={{
+        backgroundImage:
+          "conic-gradient(rgb(255 222 104)" + // yellow
+          (100 - Math.round((timeLeft / props.timeInSeconds) * 100)) +
+          "%, rgb(202 172 66) " + // yellow-dark
+          (100 - Math.round((timeLeft / props.timeInSeconds) * 100)) +
+          "%)",
+      }}
+    >
+      <span className="font-bold text-[3rem]">{timeLeft}</span>
+    </div>
+  );
+}
+
+export default Timer;
