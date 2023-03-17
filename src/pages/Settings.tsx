11fcaa36a9bd71ec12f1454bf0f25ba6da17dde5
ChravisDeYoung <dejong_travis@yahoo.ca>
2023-03-16 22:17:27 -0400

    added confirmation modal to highscore reset

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,11 +1,14 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 import Button from "../components/Button";
 import TopRightActionIcon from "../components/TopRightActionIcon";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 function SettingsPage() {
-  const [setHighscore] = useLocalStorage("highscore", 0);
+  const [highscore, setHighscore] = useLocalStorage("highscore", 0);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
@@ -15,10 +18,40 @@ function SettingsPage() {
       </header>
 
       <section className="flex flex-col items-center">
-        <Button text="reset scores" onClick={() => setHighscore(0)} />
+        <Button text="reset scores" onClick={() => setIsOpen(true)} />
       </section>
 
       <span>&nbsp;</span>
+
+      {isOpen && (
+        <section className="bg-black h-screen w-screen z-0 absolute top-0">
+          <button
+            className="absolute right-3 top-3 rounded-full h-14 w-14 border-4 border-yellow hover:bg-yellow text-yellow hover:text-black"
+            onClick={() => setIsOpen(false)}
+          >
+            <span className="text-[2rem]">
+              <FontAwesomeIcon icon={faX} />
+            </span>
+          </button>
+
+          <h1 className="mt-20 font-bold text-5xl tracking-widest text-yellow">
+            are you sure you want to reset?
+          </h1>
+
+          <div className="my-10 text-yellow">
+            <h2 className="font-semibold mb-1 text-xl">current highscore</h2>
+            <span className="font-bold text-5xl">{highscore}</span>
+          </div>
+
+          <Button
+            text="reset"
+            onClick={() => {
+              setHighscore(0);
+              setIsOpen(false);
+            }}
+          />
+        </section>
+      )}
     </>
   );
 }
