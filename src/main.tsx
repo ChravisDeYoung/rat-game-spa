52f3cff94afb3d4e669a3da0e3f12502f112d22d
ChravisDeYoung <dejong_travis@yahoo.ca>
2023-04-08 15:27:03 -0400

    deployed on github pages and changed page title

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* can't use BrowserRouter on GitHub Pages */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
